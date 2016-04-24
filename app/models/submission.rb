93b49b1a631da8a7d1ee6a298eeb324cc3dc0139
Aleix Pellisa <aleix.pellisa@est.fib.upc.edu>
2016-04-24 10:29:03 +0000

    tasca 1 acabada: es creen submissions amb el usuari que s'ha logejat i si no n'hi ha cap no et deixa crear-la.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,6 @@
 class Submission < ActiveRecord::Base
   belongs_to :user
-  validates :user_id, presence: true
+  
+  validates_presence_of :user_id, :message => "you have to log in to create a submission." 
   validates :title, presence: true
 end
