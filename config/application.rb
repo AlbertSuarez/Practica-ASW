3861f93b744087569de9e1ab017ab93de4f2559d
Oriol <ohrioloh@gmail.com>
2016-05-16 21:52:12 +0000

    CORS PUT

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module HelloApp
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put]
       end
     end
   end
