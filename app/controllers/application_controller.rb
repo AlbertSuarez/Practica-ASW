41639b6b38cc138887980123f4b67545e7ab483c
Oriol <ohrioloh@gmail.com>
2016-05-16 20:01:46 +0000

    c9 autosave pls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
   skip_before_action :verify_authenticity_token, if: :json_request?
-  
-  config.middleware.insert_before 0, "Rack::Cors" do
-    allow do
-      origins '*'
-      resource '*', :headers => :any, :methods => [:get, :post, :options]
-    end
-  end
 
   protected
 
