d21e296eda48c6a895c8ce64b903b02887e12041
Victor <victorpm5795@gmail.com>
2016-05-27 08:53:59 +0000

    work on external authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,18 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     
-    respond_to do |format|
-      if user
-        format.html {redirect_to root_path}
-        format.json { render :json => user.oauth_token }
-      end
+    if session[:url]
+      url = session[:url] + '?token=' + user.oauth_token
+      session[:url] = nil
+      redirect_to url
+    else
+      redirect_to root_path
     end
-    
+  end
+  
+  def redirecciona
+    session[:url] = params[:url]
+    redirect_to '/auth/google_oauth2'
   end
   
   def error
